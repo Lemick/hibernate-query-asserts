8cc051026fdf73ccc1a928fc17e90e9a64a4d1b2
Mickaël Beguin <sidfloyd84@gmail.com>
2022-04-16 11:45:37 +0200

    Removed ThreadLocal safety since a test can span on multiple threads (ex: a test executing a HTTP request on the server)

--- src/main/java/com/lemick/integration/hibernate/HibernateStatementCountInspector.java
+++ src/main/java/com/lemick/integration/hibernate/HibernateStatementCountInspector.java
@@ -2,20 +2,24 @@ package com.lemick.integration.hibernate;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 
+/**
+ * NOT Thread-Safe since it is meant to be used by Spring tests that are not multi-threaded
+ * ThreadLocal does not work because a test can span on multiple threads (ex: a test executing a HTTP request on the server)
+ */
 public class HibernateStatementCountInspector implements StatementInspector {
 
-    private static final ThreadLocal<HibernateStatistics> statisticsStore = ThreadLocal.withInitial(HibernateStatistics::new);
+    private static final HibernateStatistics statistics = new HibernateStatistics();
 
     private HibernateStatementParser statementParser = new JSQLHibernateStatementParser();
 
     @Override
     public String inspect(String sql) {
-        statementParser.parseSqlStatement(sql, statisticsStore.get());
+        statementParser.parseSqlStatement(sql, statistics);
         return sql;
     }
 
     public static HibernateStatistics getStatistics() {
-        return statisticsStore.get();
+        return statistics;
     }
 
 }
